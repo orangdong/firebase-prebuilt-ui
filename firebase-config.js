60be81c6e17966cda6b7861566271b48254decd4
Aviel Naftali <dev.naftaliaviel@gmail.com>
2022-06-23 15:11:05 +0700

    first init

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "YOUR-API-KEY",
+  authDomain: "YOUR-AUTH-DOMAIN",
+  projectId: "YOUR-PROJECT-ID",
+  storageBucket: "YOUR-STORAGE-BUCKET",
+  messagingSenderId: "YOUR-MESSAGING-SENDER-ID",
+  appId: "YOUR-APP-ID",
+  measurementId: "YOUR-MEASUREMENT-ID"
+};
+
+const app = firebase.initializeApp(firebaseConfig);
